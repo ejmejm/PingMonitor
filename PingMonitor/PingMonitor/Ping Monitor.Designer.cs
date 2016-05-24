39016ffbcd9623432c90a2e067001baf5d1e0c96
Edan Meyer <ninjaedan@aol.com>
2016-05-23 21:54:18 -0500

    Monitors your ping

---
+++ PingMonitor/PingMonitor/Ping Monitor.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace PingMonitor
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Test));
+            this.currentPingLabel = new System.Windows.Forms.Label();
+            this.currentPing = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.greenLight = new System.Windows.Forms.Label();
+            this.orangeLight = new System.Windows.Forms.Label();
+            this.redLight = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // currentPingLabel
+            // 
+            resources.ApplyResources(this.currentPingLabel, "currentPingLabel");
+            this.currentPingLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.currentPingLabel.Name = "currentPingLabel";
+            this.currentPingLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // currentPing
+            // 
+            resources.ApplyResources(this.currentPing, "currentPing");
+            this.currentPing.Name = "currentPing";
+            this.currentPing.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // greenLight
+            // 
+            resources.ApplyResources(this.greenLight, "greenLight");
+            this.greenLight.Name = "greenLight";
+            // 
+            // orangeLight
+            // 
+            resources.ApplyResources(this.orangeLight, "orangeLight");
+            this.orangeLight.Name = "orangeLight";
+            // 
+            // redLight
+            // 
+            resources.ApplyResources(this.redLight, "redLight");
+            this.redLight.Name = "redLight";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Test
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.redLight);
+            this.Controls.Add(this.orangeLight);
+            this.Controls.Add(this.greenLight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.currentPing);
+            this.Controls.Add(this.currentPingLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label currentPingLabel;
+        private System.Windows.Forms.Label currentPing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label greenLight;
+        private System.Windows.Forms.Label orangeLight;
+        private System.Windows.Forms.Label redLight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PingMonitor/PingMonitor/Ping Monitor.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace PingMonitor
+{
+    public partial class Test : Form
+    {
+        private readonly Timer timer;
+        private readonly Ping pingClass;
+        private readonly IPAddress ipAddress;
+        private readonly int timeout;
+        int[] pingHistory = new int[60];
+        int secondsPassed = 0;
+
+        public Test()
+        {
+            InitializeComponent();
+            IPAddress.TryParse("216.52.241.254", out ipAddress);
+            timer = new Timer();
+            timeout = 3000;
+            pingClass = new Ping();
+            pingClass.PingCompleted += PingClassPingCompleted;
+        }
+
+        void PingClassPingCompleted(object sender, PingCompletedEventArgs e)
+        {
+            if(e != null)
+            {
+                if(e.Reply != null)
+                {
+                    RefreshPing(e.Reply);
+            
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Tick += TimerTick;
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            pingClass.SendAsyncCancel();
+            pingClass.SendAsync(ipAddress, timeout);
+        }
+
+        private void RefreshPing(PingReply r)
+        {
+            if (r.Status == IPStatus.Success)
+            {
+                if (r.RoundtripTime <= 100)
+                {
+                    currentPing.ForeColor = Color.Green;
+                }
+                else if (r.RoundtripTime <= 200)
+                {
+                    currentPing.ForeColor = Color.Orange;
+                }
+                else
+                {
+                    currentPing.ForeColor = Color.Red;
+                }
+                currentPing.Text = r.RoundtripTime.ToString() + " ms";
+                pingHistory[secondsPassed % pingHistory.Length] = (int)r.RoundtripTime;
+            }
+            else
+            {
+                currentPing.ForeColor = Color.Red;
+                currentPing.Text = "Error";
+                pingHistory[secondsPassed % pingHistory.Length] = -1;
+            }
+
+            float avgPing = 0;
+            int totalPings = 0;
+
+            for (int i = 0; i < pingHistory.Length; i++)
+            {
+                if (pingHistory[i] != -1 && pingHistory[i] != 0)
+                {
+                    avgPing += pingHistory[i];
+                    totalPings++;
+                }
+            }
+
+            avgPing /= totalPings;
+
+            if (avgPing <= 100)
+            {
+                orangeLight.Visible = false;
+                redLight.Visible = false;
+                greenLight.Visible = true;
+            }
+            else if (avgPing <= 250)
+            {
+                orangeLight.Visible = true;
+                redLight.Visible = false;
+                greenLight.Visible = false;
+            }
+            else
+            {
+                orangeLight.Visible = false;
+                redLight.Visible = true;
+                greenLight.Visible = false;
+            }
+
+            secondsPassed++;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+        private void t_Tick(object sender, EventArgs e)
+        {
+            r = p.Send(pingingIP); 
+
+            if (r.Status == IPStatus.Success)
+            {
+                if(r.RoundtripTime <= 100)
+                {
+                    currentPing.ForeColor = Color.Green;
+                }
+                else if(r.RoundtripTime <= 250)
+                {
+                    currentPing.ForeColor = Color.Orange;
+                }
+                else
+                {
+                    currentPing.ForeColor = Color.Red;
+                }
+                currentPing.Text = r.RoundtripTime.ToString() + " ms";
+                pingHistory[secondsPassed % pingHistory.Length] = (int)r.RoundtripTime;
+            }
+            else
+            {
+                currentPing.ForeColor = Color.Red;
+                currentPing.Text = "Error";
+                pingHistory[secondsPassed % pingHistory.Length] = -1;
+            }
+
+            float avgPing = 0;
+            int totalPings = 0;
+            
+            for (int i = 0; i < pingHistory.Length; i++)
+            {
+                if (pingHistory[i] != -1 && pingHistory[i] != 0)
+                {
+                    avgPing += pingHistory[i];
+                    totalPings++;
+                }
+            }
+
+            avgPing /= totalPings;
+
+            if(avgPing <= 100)
+            {
+                orangeLight.Visible = false;
+                redLight.Visible = false;
+                greenLight.Visible = true;
+            }
+            else if (avgPing <= 250)
+            {
+                orangeLight.Visible = true;
+                redLight.Visible = false;
+                greenLight.Visible = false;
+            }
+            else
+            {
+                orangeLight.Visible = false;
+                redLight.Visible = true;
+                greenLight.Visible = false;
+            }
+
+            secondsPassed++;
+        }
+         */
+    }
+}
